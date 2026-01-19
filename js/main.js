45f97883bcacdbeb35099a2377d38406128b7ac6
vladimircvetov99-web <vladimir.cvetov.99@gmail.com>
2026-01-19 16:33:01 +0600

    added files to js

--- js/main.js
+++ js/main.js
@@ -8,3 +8,18 @@
         }
     };
 }());
+
+// Burger handler
+
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header__nav');
+    const menuCloseItem = document.querySelector('.header__nav-close');
+    const menuLinks = document.querySelectorAll('.header__link');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header__nav_active');
+    });
+     menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header__nav_active');
+    });
+    }());
